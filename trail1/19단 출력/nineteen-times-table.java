909aa26768ffb5e6fa14c157f1e24e9748b2e936
choirakhyun <127165910+choirakhyun@users.noreply.github.com>
2026-05-01 12:43:46 +0900

    [Passed] 19단 출력 (237ms, 11MB)

---
+++ trail1/19단 출력/nineteen-times-table.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        for (int i = 1; i <= 19; i++) {
+            for (int j = 1; j <= 19; j++) {
+                if (j % 2 == 0) {
+                    System.out.printf("%d * %d = %d\n", i, j, i*j);
+                } else if (j == 19){
+                    System.out.printf("%d * %d = %d\n", i, j, i*j);
+                } else {
+                    System.out.printf("%d * %d = %d / ", i, j, i*j);
+                }
+            }
+        }
+    }
+}
