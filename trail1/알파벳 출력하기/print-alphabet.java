49c21f27d30462ac2a911894a6fa3865593133e0
choirakhyun <127165910+choirakhyun@users.noreply.github.com>
2026-05-03 15:11:55 +0900

    [Passed] 알파벳 출력하기 (140ms, 10MB)

---
+++ trail1/알파벳 출력하기/print-alphabet.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int cnt = 65;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                if (cnt == 90) {
+                    System.out.print((char)cnt);
+                    cnt = 65;
+                } else {
+                    System.out.print((char)cnt);
+                    cnt++;
+                }
+            }
+            System.out.println();
+        }
+    }
+}
