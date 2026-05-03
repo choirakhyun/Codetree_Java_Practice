0b31aedb38415557efec7822ee1b0e0595690b8d
choirakhyun <127165910+choirakhyun@users.noreply.github.com>
2026-05-03 15:23:02 +0900

    [Passed] ab곱 N번 구하기 (129ms, 10MB)

---
+++ trail1/ab곱 N번 구하기/a-multiple-b-n-times.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 1;
+        for (int i = 1; i <= n; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            for (int j = a; j <= b; j++) {
+                sum *= j;
+            }
+            System.out.println(sum);
+            sum = 1;
+        }
+    }
+}
