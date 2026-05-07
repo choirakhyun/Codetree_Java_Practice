17bbb176ab47192f7f2a96496bfcec2fd0344d41
choirakhyun <127165910+choirakhyun@users.noreply.github.com>
2026-05-07 10:34:04 +0900

    [Passed] 자동차 단일 거래 이익 최대화하기 (216ms, 12MB)

---
+++ trail1/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        for (int i = n-1; i >= 0; i--) {
+            for (int j = i; j >= 0; j--) {
+                if (arr[i] - arr[j] > max) {
+                    max = arr[i] - arr[j];
+                }
+            }
+        }
+        System.out.print(max);
+    }
+}
