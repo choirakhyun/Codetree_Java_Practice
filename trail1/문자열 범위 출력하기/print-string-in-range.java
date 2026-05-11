f9b543adaeb3211b0d0220da6277c04ea669f9a5
choirakhyun <127165910+choirakhyun@users.noreply.github.com>
2026-05-11 15:44:05 +0900

    [Passed] 문자열 범위 출력하기 (148ms, 10MB)

---
+++ trail1/문자열 범위 출력하기/print-string-in-range.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        System.out.print(str.substring(2, 10));
+    }
+}
